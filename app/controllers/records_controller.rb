cfd66b89ebf6c5950d1e75a9241deac66ee666a0
Pavel Beletsky <pavel.beletsky@live.com>
2020-01-17 17:18:58 -0500

    test api

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,8 +1,40 @@
 class RecordsController < ApplicationController
 
+  # GET /records/
   def index
     records = Record.all
     render json: records, except: [:created_at, :updated_at]
   end
 
+  # GET /records/:id
+  def show
+    record = Record.find(record_params[:id])
+    render json: record, except: [:created_at, :updated_at]
+  end
+
+  # POST /records/
+  def create
+    record = Record.create!(record_params)
+    render json: record, except: [:created_at, :updated_at]
+  end
+
+  # PUT /records/:id
+  def update
+    record = Record.find(record_params[:id])
+    record.update(record_params)
+    render json: record, except: [:created_at, :updated_at]
+  end
+
+  private
+  
+  def record_params
+    params.permit(:id, :group, :title, :body, :confidence, :user_id)
+  end
+
 end
+
+
+
+
+
+
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,30 @@
 class UsersController < ApplicationController
 
+  # GET /users/
   def index
     users = User.all
     render json: users, except: [:created_at, :updated_at]
+  end
+
+  # GET /users/:id
+  def show
+    user = User.find(user_params[:id])
+    # render json: user, include: [:records], except: [:created_at, :updated_at]
+    render json: user.to_json(:include => {
+      :records => {:only => [:id, :group, :title, :body, :confidence, :user_id]}
+    }, :except => [:created_at, :updated_at])
+  end
+
+  # POST /leaders/
+  def create
+    user = User.create!(user_params)
+    render json: user, except: [:created_at, :updated_at]
+  end
+
+  private
+
+  def user_params
+    params.permit(:id, :username, :password)
   end
 
 end
